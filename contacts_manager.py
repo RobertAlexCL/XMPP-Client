9cf7e0521050a68b852e6a71808179bac3057867
RobertAlexCL <rsstocast@gmail.com>
2022-08-10 06:24:51 +0200

    update

--- contacts_manager.py
+++ contacts_manager.py
@@ -34,3 +34,41 @@ class ContactsManager:
         else:
             df = pd.DataFrame(connections, columns = ['Email', 'Status'])
             print(tabulate(df, headers='keys', tablefmt='psql'))
+    
+    
+    async def newContact(self):
+        try:
+            addContact = str(await ainput("Email: "))
+            self.send_presence_subscription(pto=addContact)
+            print('The user has been added')
+        except:
+            print('Something happened adding the user, try again')
+    
+    async def contactInformation(self):
+
+        contact = str(await ainput("Email: "))
+        connections = []
+        
+        await asyncio.sleep(doze)
+
+        roster = self.client_roster.groups()
+        for group in roster:
+            for jid in roster[group]:
+                status = 'Away'
+                sub = self.client_roster[jid]['subscription']
+                name = self.client_roster[jid]['name']
+                conexions = self.client_roster.presence(jid)                           
+                for res, pres in conexions.items():
+                    if pres['status']:
+                        status = pres['status']
+                if contact == jid:
+                    connections.append([
+                        jid,
+                        name,
+                        sub,
+                        status
+                    ])
+                connections
+
+            df = pd.DataFrame(connections, columns = ['Email', 'Name', 'Suscription', 'Status'])
+            print(tabulate(df, headers='keys', tablefmt='psql'))
